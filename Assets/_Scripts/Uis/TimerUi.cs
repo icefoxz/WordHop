2e450c763b13bc01dd3a18ae9c2ccab315c08561
Leo <icefoxz@msn.com>
2023-08-24 20:06:10 +0800

    演示: Timer动画

--- Assets/_Scripts/Uis/TimerUi.cs
+++ Assets/_Scripts/Uis/TimerUi.cs
@@ -9,10 +9,21 @@ public class TimerUi : MonoBehaviour
 {
     [SerializeField]private View view;
     private TMP_Text tmp_sec { get; set; }
+    private Animator animator { get; set; }
 
     void Start()
     {
         tmp_sec = view.Get<TMP_Text>("tmp_sec");
-        Game.MessagingManager.RegEvent(GameEvents.Stage_Timer_Update, bag => tmp_sec.text = bag.GetString(0));
+        animator = view.GetComponent<Animator>();
+        Game.MessagingManager.RegEvent(GameEvents.Stage_Timer_Update, bag => ApplyCountdown(bag.GetInt(0)));
+    }
+
+    private void ApplyCountdown(int time)
+    {
+        var isHurry = time <= 5;
+        tmp_sec.text = time.ToString();
+        var state = isHurry ? "Hurry" : "Countdown";
+        // reset animator play index
+        animator.Play(state, 0, 0);
     }
 }
